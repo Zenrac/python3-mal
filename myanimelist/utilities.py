a2e6e7ce1942d38b629453612b9e89f9d1178bb7
zenrac <zenrac@outlook.fr>
2017-12-22 17:02:21 +0100

    Prevent bugs with strange birthday (31 Dec for ex)

--- myanimelist/utilities.py
+++ myanimelist/utilities.py
@@ -156,7 +156,10 @@ def parse_profile_date(text, suppress=False):
         try:
             parsed_date = datetime.datetime.strptime(text, '%b %d, %I:%M %p')
         except ValueError:
-            parsed_date = datetime.datetime.strptime(text, '%b %d, %Y %I:%M %p')
+            try:
+                parsed_date = datetime.datetime.strptime(text, '%b %d, %Y %I:%M %p')
+            except ValueError:
+                parsed_date = None
         # see if it's a month/year pairing.
         return parsed_date.date() if parsed_date is not None else None
     except:
